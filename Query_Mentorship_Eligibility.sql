35c016b4b7d94d3a532d073c3cd2b0a738f984e6
jaycecox <92542382+jaycecox@users.noreply.github.com>
2021-12-11 11:30:42 -0500

    Deliverable 2

--- Query_Mentorship_Eligibility.sql
+++ Query_Mentorship_Eligibility.sql
@@ -37,18 +37,19 @@ ORDER BY Count DESC;
 --FROM retirement_titles AS rt
 --ORDER BY rt.emp_no, rt.to_date DESC;
 
---SELECT DISTINCT ON (e.emp_no) e.emp_no,
-	--e.first_name,
-	--e.last_name,
-	--e.birth_date,
-	--de.from_date,
-	--de.to_date
-	--ti.title
---INTO mentorship_eligibility
---FROM employees AS e
---INNER JOIN dept_emp as de
-	--ON (e.emp_no = de.emp_no)
---INNER JOIN titles as ti
-	--ON (e.emp_no = ti.emp_no)
-
-
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+	ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
